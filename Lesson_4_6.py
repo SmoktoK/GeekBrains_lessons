12cf5e07ab398f1f022307a0de452e8c75116057
Dmitriy_Smokotov <smokotoff@yandex.ru>
2021-01-26 22:17:50 +0300

    del trash

--- Lesson_4_6.py
+++
@@ -1,8 +0,0 @@
-from itertools import count
-
-max_num = int(input('Введите максимальное число: '))
-for el in count(int(input('Введите стартовое число: '))):
-    if el > max_num:
-        break
-    else:
-        print(el)
