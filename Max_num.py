ae0dc2df02682f085868ff345e939ca4cadbf609
Smokotov <smokotoff@yandex.ru>
2021-04-13 10:44:21 +0300

    Поправил код

--- Max_num.py
+++ Max_num.py
@@ -2,12 +2,13 @@
 Создайте функцию, принимающую на вход 3 числа и возвращающую наибольшее из них.
 """
 
-
+nums = []
 def max_num():
     num = 0
     for num in range(3):
         num = (int(input('Введите число: ')))
-    nums = [num]
+        nums.append(num)
+    print(nums)
     print('Максимальное значение: ', max(nums))
 
 
